4a53f1fbb6ac28a89955ae35fe768b8a198d72d4
zoxibox <djzoxi@gmail.com>
2015-04-21 14:13:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 
-# if minidlna config file doesnt exist then copy default to host config volume
+# if btsync config file doesnt exist then copy default to host config volume
 if [ ! -f "/config/btsync.conf" ]; then
 
 	# copy over customised config
 	cp /tmp/btsync.conf /config/
 				
 fi
+
+# création du répertoire .sync
+mkdir -p /config/.sync
 
 # set permissions for user nobody
 chmod -R 775 /config/
