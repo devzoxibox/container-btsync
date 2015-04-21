d35ae013e59f3c61c7910b9d35366bd6e82b5917
zoxibox <djzoxi@gmail.com>
2015-04-21 14:12:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 
-exec /opt/btsync/btsync --nodaemon --webui.listen 0.0.0.0:8888
+# if minidlna config file doesnt exist then copy default to host config volume
+if [ ! -f "/config/btsync.conf" ]; then
+
+	# copy over customised config
+	cp /tmp/btsync.conf /config/
+				
+fi
+
+# set permissions for user nobody
+chmod -R 775 /config/
+chown -R nobody:users /config/
+
+exec /opt/btsync/btsync --nodaemon --config /config/btsync.conf
